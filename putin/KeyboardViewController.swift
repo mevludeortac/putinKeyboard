4a331602e9c25e220db7cfb7148904cc6f4f2208
mevludeortac <ortacmevlude@gmail.com>
2021-07-27 20:19:48 +0300

    used to extension

---
+++ putin/KeyboardViewController.swift
@@ -0,0 +1,74 @@
+//
+//  KeyboardViewController.swift
+//  putin
+//
+//  Created by MEWO on 27.07.2021.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        // Add custom view sizing constraints here
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //PUTIN BUTTON
+        let putinButton = UIButton(type: UIButton.ButtonType.system)
+        putinButton.frame = CGRect(x: 100, y: 100, width: 150, height: 100)
+        putinButton.setBackgroundImage(UIImage(named: "putin.png"), for: UIControl.State.normal)
+        putinButton.addTarget(self, action: #selector(putinTapped), for: UIControl.Event.touchUpInside)
+        view.addSubview(putinButton)
+        
+        
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton(type: .system)
+        
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+        
+        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+    
+    override func viewWillLayoutSubviews() {
+        self.nextKeyboardButton.isHidden = !self.needsInputModeSwitchKey
+        super.viewWillLayoutSubviews()
+    }
+    
+    override func textWillChange(_ textInput: UITextInput?) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+    
+    override func textDidChange(_ textInput: UITextInput?) {
+        // The app has just changed the document's contents, the document context has been updated.
+        
+        var textColor: UIColor
+        let proxy = self.textDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
+            textColor = UIColor.white
+        } else {
+            textColor = UIColor.black
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, for: [])
+    }
+    
+    @objc func putinTapped(){
+        
+        let putinProxy = textDocumentProxy as UITextDocumentProxy
+        putinProxy.insertText("put in!")
+    }
+
+}
